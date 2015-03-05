870a96ad394f88bfbe64e72bd611baacec16d5d8
Sven <Sven@Laptop-Sven.fritz.box>
2015-03-05 13:11:50 +0100

    ...

--- 24-7_FM/src/visual/Master_Frame.java
+++ 24-7_FM/src/visual/Master_Frame.java
@@ -2,6 +2,8 @@ package visual;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,31 +29,30 @@ public class Master_Frame implements ActionListener {
 		masterframe.setVisible(true);
 		masterframe.setExtendedState(masterframe.MAXIMIZED_BOTH);
 		masterframe.setDefaultCloseOperation(masterframe.EXIT_ON_CLOSE);
-
+		
 		// Content
-
+		
 		Container container1 = masterframe.getContentPane();
-		Container container2 = masterframe.getContentPane();
-		container1.setBackground(Color.blue);
+		container1.setBackground(Color.yellow);
 		container1.setSize(100, 100);
-		container2.setBackground(Color.red);
-		container2.setSize(100, 100);
+		container1.setLayout(new FlowLayout());
+		
 		// JLabel
 
 		JLabel label1 = new JLabel("1");
+		label1.setFont(new Font("ARIAL", Font.BOLD, 22));
 		JLabel label2 = new JLabel("2");
 		JLabel label3 = new JLabel("3");
 		JLabel label4 = new JLabel("4");
 		JLabel label5 = new JLabel("5");
 
-		// masterframe.add(label1);
-		// masterframe.add(label2);
-		// masterframe.add(label3);
-		// masterframe.add(label4);
-		// masterframe.add(label5);
+		 masterframe.add(label1);
+		 masterframe.add(label2);
+		 masterframe.add(label3);
+		 masterframe.add(label4);
+		 masterframe.add(label5);
 
 		container1.add(label1);
-		container2.add(label2);
 	}
 
 	@Override
