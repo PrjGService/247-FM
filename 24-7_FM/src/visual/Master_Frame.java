e36506d5525467c79e8d93e2ad523f804700008a
Sven <Sven@141.72.147.229>
2015-03-11 12:55:37 +0100

    ...

--- 24-7_FM/src/visual/Master_Frame.java
+++ 24-7_FM/src/visual/Master_Frame.java
@@ -1,9 +1,11 @@
 package visual;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
-import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,6 +13,7 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Master_Frame implements ActionListener {
 
@@ -27,32 +30,38 @@ public class Master_Frame implements ActionListener {
 		masterframe.setResizable(false);
 		masterframe.setIconImage(logo);
 		masterframe.setVisible(true);
-		masterframe.setExtendedState(masterframe.MAXIMIZED_BOTH);
-		masterframe.setDefaultCloseOperation(masterframe.EXIT_ON_CLOSE);
-		
+		masterframe.setExtendedState(Frame.MAXIMIZED_BOTH);
+		masterframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		// Content
-		
+
 		Container container1 = masterframe.getContentPane();
-		container1.setBackground(Color.yellow);
-		container1.setSize(100, 100);
-		container1.setLayout(new FlowLayout());
+
+		// JPanel
+
+		JPanel window1 = new JPanel();
+
+		// Layout-Manager
+
+		GridLayout MyLayout = new GridLayout(1, 2);
+
+		JPanel panel1 = new JPanel();
+		panel1.setBackground(Color.green);
+		JPanel panel2 = new JPanel();
+		panel2.setBackground(Color.red);
+
+		window1.add(panel1);
+		window1.add(panel2);
+		window1.setLayout(MyLayout);
+
+		String head = new String ("<html><center> 24/7 Facility-Management</html>");
 		
-		// JLabel
+		JLabel label1 = new JLabel("24/7 Facility-Management");
+		label1.setFont(new Font("ARIAL", Font.BOLD, 44));
+		label1.setHorizontalAlignment(Component.CENTER_ALIGNMENT);
 
-		JLabel label1 = new JLabel("1");
-		label1.setFont(new Font("ARIAL", Font.BOLD, 22));
-		JLabel label2 = new JLabel("2");
-		JLabel label3 = new JLabel("3");
-		JLabel label4 = new JLabel("4");
-		JLabel label5 = new JLabel("5");
-
-		 masterframe.add(label1);
-		 masterframe.add(label2);
-		 masterframe.add(label3);
-		 masterframe.add(label4);
-		 masterframe.add(label5);
-
-		container1.add(label1);
+		panel1.add(label1);
+		container1.add(window1);
 	}
 
 	@Override
