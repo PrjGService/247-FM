5c3d422a91d3996bcda56c5158acff7588b80d13
Sven <Sven@Laptop-Sven>
2015-03-16 12:42:44 +0100

    ImagePanel & Background Welcome Page

--- Gebaeudeservice/src/view/WelcomePage.java
+++ Gebaeudeservice/src/view/WelcomePage.java
@@ -1,8 +1,13 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,18 +19,31 @@ public class WelcomePage extends JFrame {
 	public WelcomePage() {
 
 		JFrame welcome = new JFrame("24/7 - Facility Management");
-		Image logo = new ImageIcon("res/logo.png").getImage();
+		Image icon = new ImageIcon("res/logo.png").getImage();
 
 		welcome.setResizable(false);
-		welcome.setIconImage(logo);
+		welcome.setIconImage(icon);
 		welcome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		BufferedImage myImage = null;
+		try {
+			myImage = ImageIO.read(new File("res/gebaeudeservice.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		welcome.setContentPane(new ImagePanel(myImage));
 
 		JLabel hintergrund = new JLabel();
 		hintergrund.setIcon(new ImageIcon("res/gebaeudeservice.jpg"));
 		hintergrund.setBounds(0, 0, MAXIMIZED_BOTH, MAXIMIZED_BOTH);
+		JLabel logo = new JLabel();
+		logo.setIcon(new ImageIcon(icon));
+		logo.setLayout(new BorderLayout());
 
 		welcome.add(hintergrund);
-		
+
 		welcome.setVisible(true);
 		welcome.setExtendedState(Frame.MAXIMIZED_BOTH);
 
