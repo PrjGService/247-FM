c9bed142a9b7192ce4c4e3ddbd3fdfec7e8c95a2
a.klapp <alexander.klapp@messtechnik-mehl.de>
2015-03-17 09:37:12 +0100

    change libs

---
+++ Gebaeudeservice/src/view/neo/LoginWindowView.java
@@ -0,0 +1,27 @@
+package view.neo;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class LoginWindowView extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6242508615880797583L;
+	
+	
+	public LoginWindowView(){
+		initUI();
+	}
+	
+	public void initUI(){
+		this.setSize(new Dimension(300,200));
+		
+		
+		
+	}
+	
+
+}
