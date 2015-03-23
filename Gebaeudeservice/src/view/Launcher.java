90922bbc83335ff7d182ffc635b3524a567aec2e
Sven <Sven@Laptop-Sven>
2015-03-23 08:55:15 +0100

    ...

--- Gebaeudeservice/src/view/Launcher.java
+++ Gebaeudeservice/src/view/Launcher.java
@@ -1,8 +1,18 @@
 package view;
 
+import javax.swing.UIManager;
+
 public class Launcher {
 
 	public static void main(String[] args) {
+		
+		 try {
+	            // Set System L&F
+	        UIManager.setLookAndFeel(
+	            UIManager.getSystemLookAndFeelClassName());
+	    }  catch (Exception e){
+	    	
+	    }
 
 		System.out.println("24/7 FM started.");
 		new WelcomePage();
