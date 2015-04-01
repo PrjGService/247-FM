02d27d65e1561e8f9998e8c45b99bc36c650eb0b
Sven <Sven@Laptop-Sven>
2015-04-01 18:09:34 +0200

    statusseite ohne logik - methoden fehlen auf button

--- Gebaeudeservice/src/view/neo/content/StatusAbfragenView.java
+++ Gebaeudeservice/src/view/neo/content/StatusAbfragenView.java
@@ -6,8 +6,11 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 
 import javax.swing.Box;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -69,7 +72,7 @@ public class StatusAbfragenView extends JXPanel {
 		auftragsid.setHorizontalAlignment(SwingConstants.HORIZONTAL);
 		auftragsid.setFont(new Font("Arial", Font.PLAIN, 16));
 		auftragsid.setForeground(Color.black);
-		auftragsid.setBackground(getBackground());
+		auftragsid.setBackground(Color.WHITE);
 		auftragsid.setPreferredSize(t);
 
 		JXPanel id = new JXPanel();
@@ -77,67 +80,77 @@ public class StatusAbfragenView extends JXPanel {
 		id.setPreferredSize(d);
 		id.add(tfname);
 		id.add(auftragsid);
+		id.setBackground(Color.white);
 
 		JXPanel north = new JXPanel();
 		north.setLayout(new BorderLayout());
+		north.setBackground(Color.white);
 
 		north.add(title, BorderLayout.NORTH);
 		north.add(id, BorderLayout.SOUTH);
 
+		Icon icon1 = new ImageIcon(new ImageIcon(("res/empty40.png"))
+				.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH));
+		Icon icon2 = new ImageIcon(new ImageIcon(("res/checked18.png"))
+				.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH));
+
 		JXPanel table = new JXPanel();
 		table.setLayout(new GridLayout(5, 2));
+		table.setBackground(Color.white);
 
 		JXLabel label2 = new JXLabel();
-		label2.setText("               Abgelehnt");
+		label2.setText("                         Abgelehnt");
 		label2.setFont(new Font("Arial", Font.PLAIN, 15));
 		label2.setForeground(Color.black);
 
 		JXLabel label3 = new JXLabel();
-		label3.setText("BILDCHEN");
+		label3.setIcon(icon2);
 		label3.setFont(new Font("Arial", Font.PLAIN, 15));
 		label3.setForeground(Color.black);
 		label3.setHorizontalAlignment(SwingConstants.HORIZONTAL);
 
 		JXLabel label4 = new JXLabel();
-		label4.setText("               Angenommen");
+		label4.setText("                         Angenommen");
 		label4.setFont(new Font("Arial", Font.PLAIN, 15));
 		label4.setForeground(Color.black);
+		label4.setBackground(Color.white);
 
 		JXLabel label5 = new JXLabel();
-		label5.setText("BILDCHEN");
+		label5.setIcon(icon1);
 		label5.setFont(new Font("Arial", Font.PLAIN, 15));
 		label5.setForeground(Color.black);
 		label5.setHorizontalAlignment(SwingConstants.HORIZONTAL);
+		label5.setBackground(Color.white);
 
 		JXLabel label6 = new JXLabel();
-		label6.setText("               In Arbeit");
+		label6.setText("                         In Arbeit");
 		label6.setFont(new Font("Arial", Font.PLAIN, 15));
 		label6.setForeground(Color.black);
 
 		JXLabel label7 = new JXLabel();
-		label7.setText("BILDCHEN");
+		label7.setIcon(icon1);
 		label7.setFont(new Font("Arial", Font.PLAIN, 15));
 		label7.setForeground(Color.black);
 		label7.setHorizontalAlignment(SwingConstants.HORIZONTAL);
 
 		JXLabel label8 = new JXLabel();
-		label8.setText("               Erledigt");
+		label8.setText("                         Erledigt");
 		label8.setFont(new Font("Arial", Font.PLAIN, 15));
 		label8.setForeground(Color.black);
 
 		JXLabel label9 = new JXLabel();
-		label9.setText("BILDCHEN");
+		label9.setIcon(icon1);
 		label9.setFont(new Font("Arial", Font.PLAIN, 20));
 		label9.setForeground(new Color(19, 123, 64));
 		label9.setHorizontalAlignment(SwingConstants.HORIZONTAL);
 
 		JXLabel label10 = new JXLabel();
-		label10.setText("               Bezahlt");
+		label10.setText("                         Bezahlt");
 		label10.setFont(new Font("Arial", Font.PLAIN, 15));
 		label10.setForeground(Color.black);
 
 		JXLabel label11 = new JXLabel();
-		label11.setText("BILDCHEN");
+		label11.setIcon(icon1);
 		label11.setFont(new Font("Arial", Font.PLAIN, 15));
 		label11.setForeground(new Color(19, 123, 64));
 		label11.setHorizontalAlignment(SwingConstants.HORIZONTAL);
@@ -156,6 +169,9 @@ public class StatusAbfragenView extends JXPanel {
 		table.add(label11);
 
 		LayoutButton button = new LayoutButton("OK");
+		button.setPreferredSize(t);
+		
+		
 
 		statusPanel.add(north, BorderLayout.BEFORE_FIRST_LINE);
 		statusPanel.add(table, BorderLayout.CENTER);
